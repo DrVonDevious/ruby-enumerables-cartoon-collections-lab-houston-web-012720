6ea66c697590296d500668354d1c56666186728f
William C Rutland <william.prometheus@gmail.com>
2020-01-05 20:26:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  num = 1
+  dwarves.each() do |dwarf|
+    puts "#{num}. #{dwarf}"
+    num += 1
+  end
+end
+
+def summon_captain_planet(calls)
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
