9432f77f2497fe336173836db8830a1871dfa3af
William C Rutland <william.prometheus@gmail.com>
2020-01-05 21:02:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.find { |f| puts f; f.include? cheese_types }
+  foods.find { |f| puts f; cheese_types.include? f }
 end
